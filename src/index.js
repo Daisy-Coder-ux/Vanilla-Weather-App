b107d39eab6d72bf1260a0889dd57f5e21d5decd
Daisy-Coder-ux <Daisyesmaeili@gmail.com>
2025-01-23 15:29:03 +0330

    inject the forecast HTML with JS

--- src/index.js
+++ src/index.js
@@ -52,7 +52,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayforecast() {
+  let forecast = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-icon">🌤</div>
+  <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature">
+      <strong>15°</strong>
+    </div>
+    <div class="weather-forecast-temperature">5°</div>
+  </div>
+</div>
+`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Tehran");
+displayforecast();
